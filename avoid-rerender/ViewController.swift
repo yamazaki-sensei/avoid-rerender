27490255b6b28fb033251d1c531e876bc9978d32
Hiraku Ohno <hiraku.ohno@integrity-healthcare.co.jp>
2018-12-06 02:09:47 +0900

    Initial Commit

---
+++ avoid-rerender/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  avoid-rerender
+//
+//  Created by Hiraku Ohno on 2018/12/06.
+//  Copyright © 2018 yamazakisensei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
